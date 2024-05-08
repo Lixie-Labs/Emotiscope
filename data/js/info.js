69b88054880ba7e5d30cd4b416a9d4ad7c022f7b
Lixie Labs <5051485+connornishijima@users.noreply.github.com>
2024-05-08 17:22:13 -0600

    Added info for "reverse color range" and "auto color cycle" toggles

--- data/js/info.js
+++ data/js/info.js
@@ -1,13 +1,15 @@
 let information = {
-	"brightness"   : "The brightness of your Emotiscope's LED display.",
-	"softness"     : "How quickly your Emotiscope's display reacts to changes; this can be used to reduce flickering effects at the cost of reaction time.",
-	"speed"        : "How quickly things on screen can move",
-	"color"        : "The base color of the current mode.",
-	"color_range"  : "How much the color can vary from the base color.",
-	"saturation"   : "The intensity of the color of the display.",
-	"blue_filter"  : "Applies a filter to the display which limits the brightness of blue colors, similar to the look of incandenscent light bulbs that were tinted by a colored film.",
-	"background"   : "The intensity of the background color.",
-	"mirror_mode"  : "Scales the display to 1/2 size and mirrors it to be symmetrical",
+	"brightness"          : "The brightness of your Emotiscope's LED display.",
+	"softness"            : "How quickly your Emotiscope's display reacts to changes; this can be used to reduce flickering effects at the cost of reaction time.",
+	"speed"               : "How quickly things on screen can move",
+	"color"               : "The base color of the current mode.",
+	"color_range"         : "How much the color can vary from the base color.",
+	"saturation"          : "The intensity of the color of the display.",
+	"blue_filter"         : "Applies a filter to the display which limits the brightness of blue colors, similar to the look of incandenscent light bulbs that were tinted by a colored film.",
+	"background"          : "The intensity of the background color.",
+	"mirror_mode"         : "Scales the display to 1/2 size and mirrors it to be symmetrical",
+	"reverse_color_range" : "Reverses the direction of the color range, meaning that if your base color is red, the color range extends towards blue instead of green.",
+	"auto_color_cycle"    : "Automatically cycles the base color to the music",
 };
 
 function show_setting_information(setting_name){
